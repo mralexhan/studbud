6613c88b8fc19d06b272fa2bf4c63cbcb2d73654
unknown <aalleexx2211@gmail.com>
2022-05-20 11:25:41 +1000

    added basic music player functionality (able to play one song, volume slider, pause and play buttons, and timeline update working)

--- public/reading.js
+++ public/reading.js
@@ -263,3 +263,73 @@ function renderSingleRef(singleRef){
   
   
 }
+
+
+
+/*javascript for music player*/
+
+/*code modified from https://codepen.io/ozzie/pen/YPOBEJ*/
+
+//counts number of songs on the page
+var numSongs = document.querySelectorAll("audio").length;
+
+var songIndex = 1;
+
+var music = document.getElementById("song" + songIndex);
+var playButton = document.getElementById("play");
+var pauseButton = document.getElementById("pause");
+var playhead = document.getElementById("elapsed");
+var timeline = document.getElementById("slider");
+var timer = document.getElementById("timer");
+var duration = music.duration;
+var volumeSlider = document.getElementById("volume");
+var prevSong = document.getElementById("prevSong");
+var nextSong = document.getElementById("nextSong");
+pauseButton.style.display = "none";
+
+//runs the timeupdate function whenever the music is updated
+music.addEventListener("timeupdate", timeUpdate, false);
+
+//updates the timer and timeline when music is playinh
+function timeUpdate() {
+
+  //grabs percentage of song that has been played
+	var playPercent = (music.currentTime / duration) * 100;
+
+  //sets the width of the line that shows how much has been played based on percentage
+	playhead.style.width = ((playPercent * 0.01) * timeline.offsetWidth) + "px";
+
+  //checks how many minutes and seconds have been played
+	var secondsIn = Math.floor(music.currentTime%60);
+  var minutesIn = Math.floor(music.currentTime/60);
+
+  //updates the little timer underneath the bar based on how much of the song has been played
+	timer.innerHTML = String(minutesIn).padStart(2, "0") + ":" + String(secondsIn).padStart(2, "0");
+
+  
+}
+
+//hides play button and shows pause button when play button is clicked
+playButton.onclick = function() {
+	music.play();
+	playButton.style.display = "none";
+	pause.style.display = "initial";
+}
+
+//hides pause button and shows play button when pause button is clicked
+pauseButton.onclick = function() {
+	music.pause();
+	playButton.style.display = "initial";
+	pause.style.display = "none";
+}
+
+
+music.addEventListener("canplaythrough", function () {
+	duration = music.duration;
+}, false);
+
+
+//volume slider javascript. ccode from https://stackoverflow.com/questions/62160275/js-audio-volume-slider
+volumeSlider.addEventListener("change", function(e){
+  music.volume = e.currentTarget.value/100;
+})
--- public/tasklist.js
+++ public/tasklist.js
@@ -13,6 +13,8 @@ toggler.addEventListener('click', () => {
   menu.classList.toggle('active');
 })
 
+
+
 //Task List js starts here
 const taskForm = document.querySelector(".newTaskForm");
 const taskColumnForm = document.querySelector(".newTaskColumnForm");
@@ -398,43 +400,68 @@ if (localStorage.length != 0){
 
 /*javascript for music player*/
 
-/*code from https://codepen.io/ozzie/pen/YPOBEJ*/
-var music = document.getElementById("music");
+/*code modified from https://codepen.io/ozzie/pen/YPOBEJ*/
+
+//counts number of songs on the page
+var numSongs = document.querySelectorAll("audio").length;
+
+var songIndex = 1;
+
+var music = document.getElementById("song" + songIndex);
 var playButton = document.getElementById("play");
 var pauseButton = document.getElementById("pause");
 var playhead = document.getElementById("elapsed");
 var timeline = document.getElementById("slider");
 var timer = document.getElementById("timer");
-var duration;
+var duration = music.duration;
+var volumeSlider = document.getElementById("volume");
+var prevSong = document.getElementById("prevSong");
+var nextSong = document.getElementById("nextSong");
 pauseButton.style.display = "none";
 
-var timelineWidth = timeline.offsetWidth - playhead.offsetWidth;
+//runs the timeupdate function whenever the music is updated
 music.addEventListener("timeupdate", timeUpdate, false);
 
+//updates the timer and timeline when music is playinh
 function timeUpdate() {
-	var playPercent = timelineWidth * (music.currentTime / duration);
-	playhead.style.width = playPercent + "px";
 
-	var secondsIn = Math.floor(((music.currentTime / duration) / 3.5) * 100);
-	if (secondsIn <= 9) {
-		timer.innerHTML = "0:0" + secondsIn;
-	} else {
-		timer.innerHTML = "0:" + secondsIn;
-	}
+  //grabs percentage of song that has been played
+	var playPercent = (music.currentTime / duration) * 100;
+
+  //sets the width of the line that shows how much has been played based on percentage
+	playhead.style.width = ((playPercent * 0.01) * timeline.offsetWidth) + "px";
+
+  //checks how many minutes and seconds have been played
+	var secondsIn = Math.floor(music.currentTime%60);
+  var minutesIn = Math.floor(music.currentTime/60);
+
+  //updates the little timer underneath the bar based on how much of the song has been played
+	timer.innerHTML = String(minutesIn).padStart(2, "0") + ":" + String(secondsIn).padStart(2, "0");
+
+  
 }
 
+//hides play button and shows pause button when play button is clicked
 playButton.onclick = function() {
 	music.play();
 	playButton.style.display = "none";
 	pause.style.display = "initial";
 }
 
+//hides pause button and shows play button when pause button is clicked
 pauseButton.onclick = function() {
 	music.pause();
 	playButton.style.display = "initial";
 	pause.style.display = "none";
 }
 
+
 music.addEventListener("canplaythrough", function () {
 	duration = music.duration;
 }, false);
+
+
+//volume slider javascript. ccode from https://stackoverflow.com/questions/62160275/js-audio-volume-slider
+volumeSlider.addEventListener("change", function(e){
+  music.volume = e.currentTarget.value/100;
+})
