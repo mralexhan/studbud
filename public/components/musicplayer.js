0b38a0e048fce2b9d66dde8f4be7f72b55c86893
unknown <aalleexx2211@gmail.com>
2022-05-26 09:15:02 +1000

    added playlist functionality and next song and previous song functionality to music player

--- public/components/musicplayer.js
+++ public/components/musicplayer.js
@@ -1,12 +1,13 @@
 /*javascript for music player*/
 
-/*code modified from https://codepen.io/ozzie/pen/YPOBEJ*/
+/*code modified and built off from https://codepen.io/ozzie/pen/YPOBEJ*/
 
 //counts number of songs on the page
 var numSongs = document.querySelectorAll("audio").length;
 
 var songIndex = 1;
 
+//grabs a variety of elements from the document to use later
 var music = document.getElementById("song" + songIndex);
 var playButton = document.getElementById("play");
 var pauseButton = document.getElementById("pause");
@@ -17,10 +18,20 @@ var duration = music.duration;
 var volumeSlider = document.getElementById("volume");
 var prevSong = document.getElementById("prevSong");
 var nextSong = document.getElementById("nextSong");
+var playlistButton = document.querySelector(".playlistButton");
+var songList = document.querySelector(".songList");
+var currentSongName = document.getElementById("currentSongName");
+var currentArtist = document.getElementById("currentArtist");
+
+//sets pause button to hidden by default
 pauseButton.style.display = "none";
 
 //runs the timeupdate function whenever the music is updated
 music.addEventListener("timeupdate", timeUpdate, false);
+//plays next song when next song button is pressed
+nextSong.addEventListener("click", playNext); 
+//plays previous song when previous song button is pressed
+prevSong.addEventListener("click", playPrev);
 
 //updates the timer and timeline when music is playinh
 function timeUpdate() {
@@ -38,7 +49,7 @@ function timeUpdate() {
   //updates the little timer underneath the bar based on how much of the song has been played
 	timer.innerHTML = String(minutesIn).padStart(2, "0") + ":" + String(secondsIn).padStart(2, "0");
 
-  
+
 }
 
 //hides play button and shows pause button when play button is clicked
@@ -56,6 +67,72 @@ pauseButton.onclick = function() {
 }
 
 
+//function that plays next song
+function playNext(){
+  playButton.style.display = "none"; //hides play button
+  pause.style.display = "initial"; //shows pause button
+  music.pause(); //stops the current song
+  music.currentTime = 0; //resets the song back to beginning
+  songIndex += 1; //increments the song index so it will play the next song
+  //if songIndex goes beyond the current number of songs, it goes back to the first one
+  if (songIndex > numSongs){
+    songIndex = 1;
+  }
+  music = document.getElementById("song" + songIndex); //changes current playing song to the next song
+
+  //changes the displayed song name and artist
+  var songInfo = music.parentNode.children[1].children;
+  currentSongName.innerHTML = songInfo[0].innerHTML
+  currentArtist.innerHTMl = songInfo[1].innerHTMl
+  music.play(); //plays the new song
+  music.addEventListener("timeupdate", timeUpdate, false);
+}
+
+//function that plays previous song
+function playPrev(){
+  playButton.style.display = "none"; //hides play button
+  pause.style.display = "initial"; //shows pause button
+  music.pause(); //stops the current song
+  music.currentTime = 0; //resets the song back to beginning
+  songIndex -= 1; //increments the song index so it will play the next song
+  //if songIndex goes beyond the current number of songs, it goes back to the first one
+  if (songIndex < 1){
+    songIndex = numSongs;
+  }
+  music = document.getElementById("song" + songIndex); //changes current playing song to the next song
+
+  //changes the displayed song name and artist
+  var songInfo = music.parentNode.children[1].children;
+  currentSongName.innerHTML = songInfo[0].innerHTML
+  currentArtist.innerHTMl = songInfo[1].innerHTMl
+  music.play(); //plays the new song
+  music.addEventListener("timeupdate", timeUpdate, false);
+}
+
+
+//makes it so every element with class "songs" will behave the same way when clicked. Code from https://www.codegrepper.com/code-examples/javascript/js+add+event+listener+to+all+elements+with+class
+document.querySelectorAll(".songs").forEach(item =>{
+  item.addEventListener("click", event =>{
+    playButton.style.display = "none"; //hides play button
+  	pause.style.display = "initial"; //shows pause button
+    music.pause(); //stops the current song
+    music.currentTime = 0; //resets the song back to beginning
+
+    let songId = item.children[2].id;
+    songIndex = parseInt(songId.substring(songId.length-1)); //changes songIndex to the index of song in playlist
+    music = document.getElementById("song" + songIndex); //changes current playing song to the next song
+  
+    //changes the displayed song name and artist
+    var songInfo = music.parentNode.children[1].children;
+    currentSongName.innerHTML = songInfo[0].innerHTML
+    currentArtist.innerHTMl = songInfo[1].innerHTMl
+    music.play(); //plays the new song
+    music.addEventListener("timeupdate", timeUpdate, false);
+  })
+})
+
+
+
 music.addEventListener("canplaythrough", function () {
 	duration = music.duration;
 }, false);
@@ -65,3 +142,10 @@ music.addEventListener("canplaythrough", function () {
 volumeSlider.addEventListener("change", function(e){
   music.volume = e.currentTarget.value/100;
 })
+
+
+//Makes the songlist show up and disappear when playlist button is clicked
+var songListOut = false;
+playlistButton.addEventListener("click", function(){
+  songList.classList.toggle("active");
+});
